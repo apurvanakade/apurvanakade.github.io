1deb960bd1f43e6017172ef58343d5792ba0b242
Apurva Nakade <apurvnakade@gmail.com>
2020-08-25 15:40:26 -0400

    customize css

---
+++ _deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -rf ../gh-pages/css/
+rm -rf ../gh-pages/images/
+rm -rf ../gh-pages/libs/
+rm -rf ../gh-pages/js/
+rm ../gh-pages/*.html
+rm ../gh-pages/search_index.json
+
+cp -r _book/* ../gh-pages/
