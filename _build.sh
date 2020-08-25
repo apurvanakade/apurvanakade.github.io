9fbd3090593c448bac688e4d197b9cdd54336f32
Apurva Nakade <apurvnakade@gmail.com>
2020-08-25 17:26:44 -0400

    cleaner css

--- _build.sh
+++ _build.sh
@@ -6,3 +6,4 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 # Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+rm -rf _book/libs/gitbook-2.6.7/css
