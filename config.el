c4cd7480b94ce58208d8e7b5857e57dc0f56f35a
Apurva Nakade <apurvnakade@gmail.com>
2021-03-12 22:03:38 -0500

    external css for ox-publish

--- config.el
+++ config.el
@@ -9,18 +9,17 @@
  :base-directory "~/github/apurvanakade.github.io/develop/"
  :base-extension "org"
  :publishing-directory "~/github/apurvanakade.github.io/develop/"
- :recursive t
  :publishing-function org-html-publish-to-html
  :headline-levels 4             ; Just the default for this project.
  :auto-preamble f
  )
 
-("org-static"
- :base-directory "~/github/apurvanakade.github.io/develop/"
- :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
- :publishing-directory "~/public_html/"
- :recursive t
- :publishing-function org-publish-attachment
- )
-("org" :components ("org-notes" "org-static"))
+;; ("org-static"
+;;  :base-directory "~/github/apurvanakade.github.io/develop/"
+;;  :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+;;  :publishing-directory "~/public_html/"
+;;  :recursive t
+;;  :publishing-function org-publish-attachment
+;;  )
+;; ("org" :components ("org-notes" "org-static"))
 ))
